eb2d4c3fe4fccb2afa9e8ac539ddcc422f2f713f
Taylor <tratliff96@outlook.com>
2019-10-04 09:33:26 -0500

    Update Permutations.py

--- Permutations.py
+++ Permutations.py
@@ -2,11 +2,11 @@ import sys
 
 #approach solutions in tree like fashion and simulate depth first search build recursively
 #my_list acts as a container for the characters we insert and remove from my_string as different permutations are built
-def permutate(my_list, my_string):
+def permutate(my_list, my_string, output_list):
   if(my_list != []): # not an empty list
     for i in range(0, len(my_list)):
       my_string += my_list.pop(i)
-      permutate(my_list, my_string) # recursive call to move to next "tree node"
+      permutate(my_list, my_string, output_list) # recursive call to move to next "tree node"
       my_list.insert(i, my_string[len(my_string) - 1:])
       my_string = my_string[:-1]
 
@@ -14,18 +14,18 @@ def permutate(my_list, my_string):
       output_string = ", ".join(output_list)
       print(output_string)
   else: # tree leaf, print and travel back up
-    print(my_string) 
+    output_list.append(my_string) 
 
 def main():
     file = open(sys.argv[1], "r")
 
     if file.mode == "r": #Check if file opened 
-        lines = file.readlines()
+        lines = file.read().splitlines()
         for string in lines: #Permutate and print each output for each line in the list
             string_list = list(string)
             string_list.sort() #My permutate algorithm requires characters of a string to be in ascending order at start
 
-            permutate(string_list, "")
+            permutate(string_list, "", [])
     else:
         print("File passed as argument could not be opened.")
 
